ff8671f72e9b86f0b05dab98c1ea0d08e261ae02
denis554 <denisignatenko554@gmail.com>
2017-03-29 16:46:38 -0400

    AckOnError Javadoc Polishing
    http://stackoverflow.com/questions/43085071/kafka-consumer-exception-and-offset-commits/43094537?noredirect=1#comment73287875_43094537

--- spring-kafka/src/main/java/org/springframework/kafka/listener/config/ContainerProperties.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/config/ContainerProperties.java
@@ -350,13 +350,17 @@ public class ContainerProperties {
 	}
 
 	/**
-	 * Set whether the container should ack messages that throw exceptions or not. This
-	 * works in conjunction with {@link #ackMode} and is effective only when auto ack is
-	 * false; it is not applicable to manual acks. When this property is set to
-	 * {@code true}, all messages handled will be acked. When set to {@code false}, acks
-	 * will be produced only for successful messages. This allows a component that starts
-	 * throwing exceptions consistently to resume from the last successfully processed
-	 * message. Manual acks will be always be applied.
+	 * Set whether or not the container should commit offsets (ack messages) where the
+	 * listener throws exceptions. This works in conjunction with {@link #ackMode} and is
+	 * effective only when the kafka property {@code enable.auto.commit} is {@code false};
+	 * it is not applicable to manual ack modes. When this property is set to {@code true}
+	 * (the default), all messages handled will have their offset committed. When set to
+	 * {@code false}, offsets will be committed only for successfully handled messages.
+	 * Manual acks will always be applied. Bear in mind that, if the next message is
+	 * successfully handled, its offset will be committed, effectively committing the
+	 * offset of the failed message anyway, so this option has limited applicability.
+	 * Perhaps useful for a component that starts throwing exceptions consistently;
+	 * allowing it to resume when restarted from the last successfully processed message.
 	 * @param ackOnError whether the container should acknowledge messages that throw
 	 * exceptions.
 	 */
