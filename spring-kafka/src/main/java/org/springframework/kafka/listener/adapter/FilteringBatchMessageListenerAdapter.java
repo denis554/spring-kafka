36cf2bba450c872ae65b2a08d791635f7efa8de0
denis554 <denisignatenko554@gmail.com>
2017-04-25 11:03:15 -0400

    GH-280: Polishing - Restore ackDiscarded
    Restore `ackDiscarded` to batch filtering listener adapter.

--- spring-kafka/src/main/java/org/springframework/kafka/listener/adapter/FilteringBatchMessageListenerAdapter.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/adapter/FilteringBatchMessageListenerAdapter.java
@@ -41,6 +41,8 @@ public class FilteringBatchMessageListenerAdapter<K, V>
 		extends AbstractFilteringMessageListener<K, V, BatchMessageListener<K, V>>
 		implements BatchAcknowledgingConsumerAwareMessageListener<K, V> {
 
+	private final boolean ackDiscarded;
+
 	/**
 	 * Create an instance with the supplied strategy and delegate listener.
 	 * @param delegate the delegate.
@@ -49,6 +51,23 @@ public class FilteringBatchMessageListenerAdapter<K, V>
 	public FilteringBatchMessageListenerAdapter(BatchMessageListener<K, V> delegate,
 			RecordFilterStrategy<K, V> recordFilterStrategy) {
 		super(delegate, recordFilterStrategy);
+		this.ackDiscarded = false;
+	}
+
+	/**
+	 * Create an instance with the supplied strategy and delegate listener.
+	 * When 'ackDiscarded' is false, and all messages are filtered, an empty list
+	 * is passed to the delegate (so it can decide whether or not to ack); when true, a
+	 * completely filtered batch is ack'd by this class, and no call is made to the delegate.
+	 * @param delegate the delegate.
+	 * @param recordFilterStrategy the filter.
+	 * @param ackDiscarded true to ack (commit offset for) discarded messages when the
+	 * listener is configured for manual acks.
+	 */
+	public FilteringBatchMessageListenerAdapter(BatchMessageListener<K, V> delegate,
+			RecordFilterStrategy<K, V> recordFilterStrategy, boolean ackDiscarded) {
+		super(delegate, recordFilterStrategy);
+		this.ackDiscarded = ackDiscarded;
 	}
 
 	@Override
@@ -61,7 +80,8 @@ public class FilteringBatchMessageListenerAdapter<K, V>
 			}
 		}
 		if (consumerRecords.size() > 0 || this.delegateType.equals(ListenerType.ACKNOWLEDGING_CONSUMER_AWARE)
-				|| this.delegateType.equals(ListenerType.CONSUMER_AWARE)) {
+				|| this.delegateType.equals(ListenerType.CONSUMER_AWARE)
+				|| (!this.ackDiscarded && this.delegateType.equals(ListenerType.ACKNOWLEDGING))) {
 			switch (this.delegateType) {
 				case ACKNOWLEDGING_CONSUMER_AWARE:
 					this.delegate.onMessage(consumerRecords, acknowledgment, consumer);
@@ -74,6 +94,11 @@ public class FilteringBatchMessageListenerAdapter<K, V>
 					break;
 				case SIMPLE:
 					this.delegate.onMessage(consumerRecords);
+			}
+		}
+		else {
+			if (this.ackDiscarded && acknowledgment != null) {
+				acknowledgment.acknowledge();
 			}
 		}
 	}
---
+++ spring-kafka/src/test/java/org/springframework/kafka/adapter/FilteringAdapterTests.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.kafka.adapter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+
+import org.springframework.kafka.listener.BatchAcknowledgingMessageListener;
+import org.springframework.kafka.listener.adapter.FilteringBatchMessageListenerAdapter;
+import org.springframework.kafka.support.Acknowledgment;
+
+/**
+ * @author Gary Russell
+ * @since 2.0
+ *
+ */
+public class FilteringAdapterTests {
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testBatchFilter() throws Exception {
+		BatchAcknowledgingMessageListener<String, String> listener = mock(BatchAcknowledgingMessageListener.class);
+		FilteringBatchMessageListenerAdapter<String, String> adapter =
+				new FilteringBatchMessageListenerAdapter<String, String>(listener, r -> false);
+		List<ConsumerRecord<String, String>> consumerRecords = new ArrayList<>();
+		final CountDownLatch latch = new CountDownLatch(1);
+		willAnswer(i -> {
+			latch.countDown();
+			return null;
+		}).given(listener).onMessage(any(List.class), any(Acknowledgment.class));
+		Acknowledgment ack = mock(Acknowledgment.class);
+		adapter.onMessage(consumerRecords, ack, null);
+		assertThat(latch.await(10, TimeUnit.SECONDS)).isTrue();
+		verify(ack, never()).acknowledge();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testBatchFilterAckDiscard() throws Exception {
+		BatchAcknowledgingMessageListener<String, String> listener = mock(BatchAcknowledgingMessageListener.class);
+		FilteringBatchMessageListenerAdapter<String, String> adapter =
+				new FilteringBatchMessageListenerAdapter<String, String>(listener, r -> false, true);
+		List<ConsumerRecord<String, String>> consumerRecords = new ArrayList<>();
+		final CountDownLatch latch = new CountDownLatch(1);
+		adapter.onMessage(consumerRecords, () -> latch.countDown(), null);
+		assertThat(latch.await(10, TimeUnit.SECONDS)).isTrue();
+		verify(listener, never()).onMessage(any(List.class), any(Acknowledgment.class));
+	}
+
+}
