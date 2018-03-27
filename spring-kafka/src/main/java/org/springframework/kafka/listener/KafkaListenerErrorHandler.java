e420160f41f25dafd5ec17e231c0b9bd7278d27c
denis554 <denisignatenko554@gmail.com>
2018-03-27 11:57:41 -0400

    Fix typo in the KafkaListenerErrorHandler
    **Chery-pick to 2.0.x and 1.3.x**

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaListenerErrorHandler.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaListenerErrorHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,8 @@ import org.springframework.messaging.Message;
  *
  * @author Venil Noronha
  * @author Gary Russell
+ * @author Artem Bilan
+ *
  * @since 1.3
  */
 @FunctionalInterface
@@ -38,7 +40,7 @@ public interface KafkaListenerErrorHandler {
 	 * @param message the spring-messaging message.
 	 * @param exception the exception the listener threw, wrapped in a
 	 * {@link ListenerExecutionFailedException}.
-	 * @return the return value is ignored unless the annoated method has a
+	 * @return the return value is ignored unless the annotated method has a
 	 * {@code @SendTo} annotation.
 	 * @throws Exception an exception which may be the original or different.
 	 */
@@ -50,12 +52,13 @@ public interface KafkaListenerErrorHandler {
 	 * @param exception the exception the listener threw, wrapped in a
 	 * {@link ListenerExecutionFailedException}.
 	 * @param consumer the consumer.
-	 * @return the return value is ignored unless the annoated method has a
+	 * @return the return value is ignored unless the annotated method has a
 	 * {@code @SendTo} annotation.
 	 * @throws Exception an exception which may be the original or different.
 	 */
 	default Object handleError(Message<?> message, ListenerExecutionFailedException exception,
 			Consumer<?, ?> consumer) throws Exception {
+
 		return handleError(message, exception);
 	}
 
