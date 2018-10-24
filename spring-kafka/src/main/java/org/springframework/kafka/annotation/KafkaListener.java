ea560183e8afedc1cca7cf573bb07bc3bd0d090c
denis554 <denisignatenko554@gmail.com>
2018-10-24 16:39:26 -0500

    Update documentation on KafkaListener annotation
    Expand id documentation to clarify that it will override the group id specified in the configuration by default.

--- spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListener.java
+++ spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListener.java
@@ -93,6 +93,9 @@ public @interface KafkaListener {
 	/**
 	 * The unique identifier of the container managing for this endpoint.
 	 * <p>If none is specified an auto-generated one is provided.
+	 * <p>Note: When provided, this value will override the group id property
+	 * in the consumer factory configuration, unless {@link #idIsGroup()}
+	 * is set to false.
 	 * @return the {@code id} for the container managing for this endpoint.
 	 * @see org.springframework.kafka.config.KafkaListenerEndpointRegistry#getListenerContainer(String)
 	 */
